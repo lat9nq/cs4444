9911d31b26efe577677f9fb01d1326633ef86ed3
lat9nq <lat9nq@virginia.edu>
2018-02-26 22:57:35 -0500

    Bug fixes

--- hw2/pblend.sh
+++ hw2/pblend.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#SBATCH -w slurm[2]
+#SBATCH -t 1:00:00
+#SBATCH -n 1
+
 BLENDER="/usr/bin/blender"
 
 FILE=$1
@@ -12,7 +16,7 @@ DIFF=`echo "($END - $START + 1) / $CHILDREN" | bc`
 for ((i=0; i<$CHILDREN; i++)); do
 	S=`echo "$i * $DIFF + $START" | bc`
 	E=`echo "$S + $DIFF - 1" | bc`
-	$BLENDER -b $FILE -s $S -e $E -a -t 1 -o out/\#\#.png -F PNG&
+	srun $CHILDREN $BLENDER -b $FILE -s $S -e $E -t 1 -o ./out/\#\#.png -F PNG -a&
 done;
 
 
