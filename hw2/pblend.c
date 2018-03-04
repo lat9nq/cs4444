06c239c30f37db9e400219faaee17b6c7d5ba664
lat9nq <lat9nq@virginia.edu>
2018-03-04 16:44:36 -0500

    Added logic for stagger mode

--- hw2/pblend.c
+++ hw2/pblend.c
@@ -88,81 +88,89 @@ int main(int argc, char * argv[]) {
 	// array of all the children
 	int children[child_count];
 
-	if (mode == ORDERED) {
-
-		int pid;
-		int this;
-		int next;
-		char start_s[32]; // string form of start frame index
-		char end_s[32]; // string form of end frame index
-
-		next = start - 1;
-
-		// for each child blender process requested
-		for (i = 0; i < child_count; i++) {
-			// fork/exec
-			pid = fork();
-			if (pid == 0) { // if child
-				this = next + 1;
-				next = start + i * frames / child_count - 1;
-				// convert to strings for arguments
-				sprintf(start_s, "%d", start + i * frames / child_count);
-				sprintf(end_s, "%d", start + (i+1) * frames / child_count - 1);
-				// ouptut the command to stderr before running
-				fprintf(stderr, "%s -t1 -b %s -s %s -e %s -a\n", BLENDER, filename, start_s, end_s);
-				execl(BLENDER, "-t1", "-b", filename, "-s", start_s, "-e", end_s, "-a",
-						(char *)NULL);
-
-				// if we're here, something went wrong!
-				fprintf(stderr, "error: `%s' is not a valid executable\n", BLENDER);
-				exit(1);
-			}
-			else {
-				// if parent, remember who the child is
-				children[i] = pid;
-			}
-		}
-
-		// wait for all the children to finish
-		int err; // error code
-		for (i = 0; i < child_count; i++) {
-			waitpid(children[i], &err, 0);
-			if (err) { // on error, stop
-				fprintf(stderr, "error: child %d exited with status code %d\n", children[i], err);
-				exit(0);
-			}
-			else
-				fprintf(stderr, "child %d finished!\n", children[i]);
-		}
-
-		char arg_rate[8]; // string of frame rate
-		char arg_start[8]; // string of start frame
-
-		// append working directory to file names
-		if (blend_out[0] != '/') {
-			strcpy(temp, pwd);
-			strcat(temp, "/");
-			strcat(temp, blend_out);
-			strcpy(blend_out, temp);
-		}
-
-		// convert numbers to strings
-		sprintf(arg_rate, "%d", rate);
-		sprintf(arg_start, "%d", start);
-
-		// exec, no fork needed
-		fprintf(stderr, "%s -r %s -start_number %s -i %s -vcodec mpeg4 output.mp4\n", FFMPEG, arg_rate, arg_start, blend_out);
-		execl(FFMPEG, "ffmpeg", "-i", blend_out, "-r", arg_rate, "-start_number", arg_start,
-				"-vcodec", "mpeg4", "output.mp4", (char *)NULL);
-		fprintf(stderr, "error: `%s' is not a valid executable\n", FFMPEG);
-
-		return 0;
-	}
-
 	int pid;
+	int this = 0;
+	int next = 0;
+	int stagger = 0;
+	char start_s[32]; // string form of start frame index
+	char end_s[32]; // string form of end frame index
+	char stagger_s[32];
+
+	if (mode == ORDERED)
+		next = start - 1;
+	else if (mode == STAGGERED)
+		stagger = child_count;
+
+	// for each child blender process requested
 	for (i = 0; i < child_count; i++) {
+		if (mode == ORDERED) {
+			this = next + 1;
+			next = start + (i+1) * frames / child_count - 1;
+		}
+		else if (mode == STAGGERED) {
+			this = i + 1;
+			if (i >= end)
+				break;
+		}
+		// fork/exec
 		pid = fork();
+		if (pid == 0) { // if child
+			// convert to strings for arguments
+			sprintf(start_s, "%d", this);
+			sprintf(end_s, "%d", next);
+			sprintf(stagger_s, "%d", stagger);
+			// ouptut the command to stderr before running
+			if (mode == ORDERED) {
+				fprintf(stderr, "%s -t1 -b %s -s %s -e %s -a\n", BLENDER, filename, start_s, end_s);
+				//execl(BLENDER, "-t1", "-b", filename, "-s", start_s, "-e", end_s, "-a", (char *)NULL);
+			}
+			else if (mode == STAGGERED) {
+				fprintf(stderr, "%s -t1 -b %s -s %s -j %s -a\n", BLENDER, filename, start_s, stagger_s);
+				execl(BLENDER, "-t1", "-b", filename, "-s", start_s, "-j", stagger_s, "-a", (char *)NULL);
+			}
+
+			// if we're here, something went wrong!
+			fprintf(stderr, "error: `%s' is not a valid executable\n", BLENDER);
+			exit(1);
+		}
+		else {
+			// if parent, remember who the child is
+			children[i] = pid;
+		}
 	}
+
+	// wait for all the children to finish
+	int err; // error code
+	for (i = 0; i < child_count; i++) {
+		waitpid(children[i], &err, 0);
+		if (err) { // on error, stop
+			fprintf(stderr, "error: child %d exited with status code %d\n", children[i], err);
+			exit(0);
+		}
+		else
+			fprintf(stderr, "child %d finished!\n", children[i]);
+	}
+
+	char arg_rate[8]; // string of frame rate
+	char arg_start[8]; // string of start frame
+
+	// append working directory to file names
+	if (blend_out[0] != '/') {
+		strcpy(temp, pwd);
+		strcat(temp, "/");
+		strcat(temp, blend_out);
+		strcpy(blend_out, temp);
+	}
+
+	// convert numbers to strings
+	sprintf(arg_rate, "%d", rate);
+	sprintf(arg_start, "%d", start);
+
+	// exec, no fork needed
+	fprintf(stderr, "%s -r %s -start_number %s -i %s -vcodec mpeg4 output.mp4\n", FFMPEG, arg_rate, arg_start, blend_out);
+	execl(FFMPEG, "ffmpeg", "-i", blend_out, "-r", arg_rate, "-start_number", arg_start,
+			"-vcodec", "mpeg4", "output.mp4", (char *)NULL);
+	fprintf(stderr, "error: `%s' is not a valid executable\n", FFMPEG);
 
 	return 0;
 }
